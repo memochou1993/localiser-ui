f99b59f209ab5310f9c151bd264f16f4df053e87
Memo Chou <memochou1993@gmail.com>
2021-07-25 16:36:47 +0800

    Add button to toggle drawer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,52 @@
 <template>
   <q-layout
-    view="lhr lpr lfr"
+    view="hhh lpr fff"
   >
-    <TheDrawer
-      v-if="false"
-    />
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          dense
+          flat
+          icon="mdi-menu"
+          round
+          @click="toggleDrawer"
+        />
+        <q-toolbar-title>
+          Localiser
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-drawer
+      v-if="isAuthenticated"
+      v-model="state.drawer"
+      :width="240"
+      class="bg-primary"
+      show-if-above
+    >
+      <q-list>
+        <q-item
+          class="text-caption text-white"
+          clickable
+          href="https://github.com/quasarframework/"
+          tag="a"
+        >
+          <q-item-section
+            avatar
+          >
+            <q-icon
+              name="mdi-file-table-outline"
+            />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label
+              class="text-body2"
+            >
+              Projects
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -12,12 +54,30 @@
 </template>
 
 <script>
-import TheDrawer from '@/components/TheDrawer.vue';
+import {
+  computed,
+  reactive,
+} from 'vue';
+import {
+  useStore,
+} from 'vuex';
 
 export default {
   name: 'LayoutDefault',
-  components: {
-    TheDrawer,
+  setup() {
+    const store = useStore();
+    const state = reactive({
+      drawer: true,
+    });
+    const isAuthenticated = computed(() => !!store.state.token);
+    const toggleDrawer = () => {
+      state.drawer = !state.drawer;
+    };
+    return {
+      state,
+      isAuthenticated,
+      toggleDrawer,
+    };
   },
 };
 </script>
