615a8c636764dbbcf260ec615f9e294cb45c319a
Memo Chou <memochou1993@gmail.com>
2021-08-02 21:59:31 +0800

    Update styles of ProjectList component

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,47 +1,37 @@
 <template>
   <q-list>
+    <q-separator
+      spaced
+    />
     <template
-      v-for="(project, i) in projects"
+      v-for="(project) in projects"
       :key="project.id"
     >
-      <q-separator
-        v-show="i === 0"
-        spaced
-      />
       <q-item>
         <q-item-section
           class="text-weight-regular"
         >
-          <q-item-label
-            class="text-subtitle2 text-weight-regular q-mb-sm"
+          <div
+            class="text-subtitle2 text-weight-regular q-mb-md"
           >
             <router-link
               :to="{ name: 'project', params: { projectId: project.id } }"
               class="text-secondary underline-none underline-hover"
               v-text="project.name"
             />
-          </q-item-label>
-          <q-item-label
-            v-show="project.description"
-            caption
-            class="q-mb-sm"
-            v-text="project.description"
-          />
-          <q-item-label>
-            <template
+          </div>
+          <div>
+            <q-chip
               v-for="(language) in project.languages"
               :key="language.id"
-            >
-              <q-chip
-                :label="language.name"
-                class="q-ma-none q-mr-sm cursor-pointer"
-                color="red"
-                dense
-                outline
-                square
-              />
-            </template>
-          </q-item-label>
+              :label="language.name"
+              class="q-ma-none q-mr-sm cursor-pointer"
+              color="red"
+              dense
+              outline
+              square
+            />
+          </div>
         </q-item-section>
       </q-item>
       <q-separator
