f8e43b0bfc9aaa70e4d8bf69624965fbd2a0b5ff
Memo Chou <memochou1993@gmail.com>
2021-08-04 01:58:39 +0800

    Fix input state of AppFilter component

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -5,20 +5,38 @@
     flat
   >
     <q-input
+      v-model="input"
       autofocus
       borderless
       dense
-      placeholder="Search"
       @update:model-value="(v) => $emit('update', v)"
-    />
+    >
+      <template
+        #append
+      >
+        <q-icon
+          name="mdi-magnify"
+        />
+      </template>
+    </q-input>
   </q-card>
 </template>
 
 <script>
+import {
+  ref,
+} from 'vue';
+
 export default {
   name: 'AppFilter',
   emits: [
     'update',
   ],
+  setup() {
+    const input = ref('');
+    return {
+      input,
+    };
+  },
 };
 </script>
