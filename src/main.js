bf27cb34af3420fbf295d7f045e1517914f201fe
Memo Chou <memochou1993@gmail.com>
2021-08-22 02:07:17 +0800

    Fix circular dependency

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue';
 import { Quasar, Notify } from 'quasar';
 import iconSet from 'quasar/icon-set/mdi-v4';
+import {
+  setInterceptors,
+} from '@/plugins/axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import i18n from './plugins/i18n';
 import '@quasar/extras/mdi-v4/mdi-v4.css';
 import './styles/quasar.sass';
+
+setInterceptors(store, router);
 
 createApp(App)
   .use(Quasar, {
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,30 +1,29 @@
 /* eslint-disable no-param-reassign */
 import axios from 'axios';
-import store from '@/store';
-import router from '@/router';
 
 const client = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
 
-client.interceptors.request.use((config) => {
-  const { token } = store.state;
-  const decoded = Buffer.from(token || '', 'base64').toString();
-  if (decoded) {
-    config.headers.Authorization = `Bearer ${decoded}`;
-  }
-  return config;
-});
-
-client.interceptors.response.use(
-  (res) => res,
-  async (e) => {
-    if (e?.response?.status === 401) {
-      await store.commit('reset');
-      await router.push({ name: 'login' });
+export const setInterceptors = (store, router) => {
+  client.interceptors.request.use((config) => {
+    const { token } = store.state;
+    const decoded = Buffer.from(token || '', 'base64').toString();
+    if (decoded) {
+      config.headers.Authorization = `Bearer ${decoded}`;
     }
-    return Promise.reject(e);
-  },
-);
+    return config;
+  });
+  client.interceptors.response.use(
+    (res) => res,
+    async (e) => {
+      if (e?.response?.status === 401) {
+        await store.commit('reset');
+        await router.push({ name: 'login' });
+      }
+      return Promise.reject(e);
+    },
+  );
+};
 
 export default client;
